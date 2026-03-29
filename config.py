91fd794b1ff8497fddd05831abfcaa84e958e621
iamnotknowma-create <iamnotknowma@gmail.com>
2026-03-29 21:52:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "27293287"
+api_hash = "052041b03a7028b3bd11da3143cffa0e"
+bot_token = "8715405308:AAG1RbPZvqEpPYSH5nPO9Sn7gZZWjoJTPCU"
+auth_users = [7533484450]
+sudo_users = [7533484450]
